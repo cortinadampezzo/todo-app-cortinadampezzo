9dd7296130754cf1fd7abbe7e4fb52c0e335adf1
cortinadampezzo <kisgans@gmail.com>
2019-03-14 09:45:06 +0100

    start project

---
+++ src/main/java/com/codecool/todoapp/TodoAppCortinadampezzoApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppCortinadampezzoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppCortinadampezzoApplication.class, args);
+    }
+
+}
