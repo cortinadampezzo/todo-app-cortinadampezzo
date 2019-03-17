00ac09f66071817f11c156a2ae74bb42b0785976
cortinadampezzo <kisgans@gmail.com>
2019-03-17 15:10:13 +0100

    implemented auth0, but scopes doesn't work

--- src/main/java/com/codecool/todoapp/authentication/AppConfig.java
+++ src/main/java/com/codecool/todoapp/authentication/AppConfig.java
@@ -1,4 +1,50 @@
 package com.codecool.todoapp.authentication;
 
-public class AppConfig {
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@EnableWebSecurity
+@Configuration
+public class AppConfig extends WebSecurityConfigurerAdapter {
+
+    @Value(value = "https://todoapp/api")
+    private String apiAudience;
+    @Value(value = "https://dev-8p1iwwey.eu.auth0.com/")
+    private String issuer;
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*", "http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("Authorization");
+        configuration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        JwtWebSecurityConfigurer
+                .forRS256(apiAudience, issuer)
+                .configure(http)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
+                .antMatchers(HttpMethod.POST, "/list").authenticated()
+                .antMatchers(HttpMethod.POST, "/addTodo").hasAuthority("read:admin");
+    }
 }
